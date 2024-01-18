f4e5b8c876f0cd20ea43664f9709edcf53b76e2a
Khanh-Kayden <nguyen.khanh1@northeastern.edu>
2024-01-18 18:43:13 -0500

    part_1 basics

--- HW1/Part_1/scripts/main.js
+++ HW1/Part_1/scripts/main.js
@@ -1,35 +1,3 @@
-// const myHeading = document.querySelector("h1");
-// myHeading.textContent = "Hello World!"
-
-// let myVariable;
-
-// myVariable = "Bob";
-
-// myVariable = "steve";
-
-// let iceCream = "chocolate";
-// if (iceCream === "chocolate") {
-//     alert("Yay, I love chocolate ice cream!");
-// } else {
-//     alert("Awwww, but chocolate is my favorite...");
-// }
-
-// function multiply(num1, num2) {
-//     let result = num1 * num2;
-//     return result;
-// }
-
-// test('mul 4 by 7', () => {
-//     expect(multiply(4,7)).toBe(28);
-// })
-
-// document.querySelector("html").addEventListener("click", function() {
-//     alert("Ouch! Stop poking me!");
-// })
-
-
-
-
 const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
@@ -41,24 +9,26 @@ myImage.onclick = () => {
   }
 };
 
-let myButton = documnet.querySelector("button");
+let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
 function setUserName() {
-    const myName = prompt("Please enter your name.");
-    if (!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem("name", myName);
-      myHeading.textContent = `Mozilla is cool, ${myName}`;
-    }
+  const myName = promt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }  
 }
 
 if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
 }
 
-myButton.onclick = setUserName();
+myButton.onclick = function() {
+  setUserName();
+}
--- HW1/Part_1/scripts/test.js
+++
@@ -1,3 +0,0 @@
-// test('mul 4 by 7', () => {
-//     expect(multiply(4,7)).toBe(28);
-// });
